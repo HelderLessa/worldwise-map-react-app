59396cd4d9eff98527b0ef0c4025d3a66f074379
Helder Lessa <116907887+HelderLessa@users.noreply.github.com>
2023-08-18 14:42:04 -0300

    Add files via upload

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,53 @@
+import { useCities } from "../contexts/CitiesContext";
+import styles from "./CityItem.module.css";
+import { Link } from "react-router-dom";
+
+const flagemojiToPNG = (flag) => {
+  var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+    .join("");
+  return (
+    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+  );
+};
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
+export default function CityItem({ city }) {
+  const { currentCity, deleteCity } = useCities();
+  const {
+    cityName,
+    emoji,
+    date,
+    id,
+    position: { lat, lng },
+  } = city;
+
+  function handleClick(e) {
+    e.preventDefault();
+    deleteCity(id);
+  }
+
+  return (
+    <li>
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${lat}&lng=${lng}`}
+      >
+        <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button onClick={handleClick} className={styles.deleteBtn}>
+          &times;
+        </button>
+      </Link>
+    </li>
+  );
+}
